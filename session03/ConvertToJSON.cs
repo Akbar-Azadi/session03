7bfbf8848c7a8debaa8e6db13cbaf7f1d2c6847b
AkbarAzadi <prg0act@gmail.com>
2025-01-06 19:31:45 +0330

    new file

---
+++ session03/ConvertToJSON.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace session03
+{
+    internal class ConvertToJSON
+    {
+        public string Execute(object obj)
+        {
+            var WhatsType = obj.GetType();
+            string Res = "{";
+            foreach (var x in WhatsType.GetProperties())
+            {
+                Res += $"{x.Name}";
+            }
+            return Res + "}"; 
+        }
+    }
+}
